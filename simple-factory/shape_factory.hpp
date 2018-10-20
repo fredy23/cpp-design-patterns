0fe32924dcab9b2379b2f415380b6970f19a8de2
Tomasz Wójcik <tommywoj@gmail.com>
2018-10-20 13:06:48 +0200

    Added const qualifiers

--- simple-factory/shape_factory.hpp
+++ simple-factory/shape_factory.hpp
@@ -17,25 +17,25 @@ class ShapeFactory
         };
 
         template<typename... Args>
-        std::unique_ptr<IShape> createShape(ShapeType p_type, Args&&... args);
+        std::unique_ptr<IShape> createShape(ShapeType p_type, Args&&... args) const;
 
     private:
         template<typename T, typename... Args>
-        std::unique_ptr<T> construct(...)
+        std::unique_ptr<T> construct(...) const
         {
             return nullptr;
         }
 
         template<typename T, typename... Args>
         std::enable_if_t<std::is_constructible<T, Args...>::value, std::unique_ptr<T>>
-        construct(Args&&... args)
+        construct(Args&&... args) const
         {
             return std::make_unique<T>(std::forward<Args>(args)...);
         }
 };
 
 template<typename... Args>
-std::unique_ptr<IShape> ShapeFactory::createShape(ShapeType p_type, Args&&... args)
+std::unique_ptr<IShape> ShapeFactory::createShape(ShapeType p_type, Args&&... args) const
 {
     if(p_type == ShapeType::TYPE_CIRCLE)
     {
